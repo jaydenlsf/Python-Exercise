a360f8511c55378c8de48d5f0034822f2e356655
jayden-lee <jaydenlsf47@gmail.com>
2021-04-22 17:16:00 +0100

    Correct exercise_4

--- exercise_4.py
+++ exercise_4.py
@@ -3,7 +3,9 @@ player_2 = (input('Player 2: ')).lower()
 
 win = {'r': 's', 'p': 'r', 's': 'p'}
 
-if player_1 == win[player_2]:
+if player_1 not in win or player_2 not in win:
+    print('Invalid option.')
+elif player_1 == win[player_2]:
     print('Player 2 wins!')
 elif player_2 == win[player_1]:
     print('Player 1 wins!')
