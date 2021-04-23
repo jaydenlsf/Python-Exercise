0cbad248ca7591b77c4f84b298fde010e2918db6
jayden-lee <jaydenlsf47@gmail.com>
2021-04-23 11:55:53 +0100

    Modified times_table.py

--- gitlab-exercises/easy/times_table.py
+++ gitlab-exercises/easy/times_table.py
@@ -1,5 +1,6 @@
-import pprint
-
 n = int(input('Enter a number: '))
 table = list(list(range(1*i, (n+1)*i, i)) for i in range(1, n+1))
-pprint.pprint(table)
+
+for i in table:
+    i = [str(j).rjust(len(str(table[-1][-1])) + 1) for j in i]
+    print(''.join(i))
