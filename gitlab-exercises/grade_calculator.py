ed731b2dc68e40c6a26391f447dbe064bc8cc3db
jayden-lee <jaydenlsf47@gmail.com>
2021-04-23 10:58:58 +0100

    Complete grade_calculator

---
+++ gitlab-exercises/grade_calculator.py
@@ -0,0 +1,22 @@
+print('Enter your marks for Maths, Chemistry, and Physics')
+maths = int(input('Maths: '))
+chem = int(input('Chemistry: '))
+physics = int(input('Physics: '))
+print('\n')
+
+avg = (maths + chem + physics) / 3
+
+grade_dict = {'D': range(40, 50), 'C': range(50, 60), 'B': range(60, 70), 'A': range(70, 100)}
+
+
+
+for grade, mark in grade_dict.items():
+    if avg < 40:
+        print('You failed!')
+        break
+    elif avg > 100:
+        print('Invalid entry(s)!')
+        break
+    elif avg in mark:
+        print('\nAverage:', round(avg, 2), '\n')
+        print(f'Your grade: {grade}')
