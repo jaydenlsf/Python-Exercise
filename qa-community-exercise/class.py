70405e5a0ab230055efdfcbb517f3a890aede1d1
jayden-lee <jaydenlsf47@gmail.com>
2021-04-27 12:23:56 +0100

    Modify average score function args

--- qa-community-exercise/class.py
+++ qa-community-exercise/class.py
@@ -5,12 +5,12 @@ class Students:
         self.age = age
         self.class_name = class_name
 
-    def get_avg_score(self, score_1, score_2, score_3):
+    def get_avg_score(self, *test_scores):
 
-        return (score_1 + score_2 + score_3) / 3
+        return sum(test_scores) / len(test_scores)
 
 
 student_1 = Students("Jayden", 24, "DevOps")
 print(student_1.name)
 
-print(student_1.get_avg_score(20, 30, 20))
+print(student_1.get_avg_score(20, 30, 20, 50, 60))
