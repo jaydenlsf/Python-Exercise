dabc099e54369dc3b9a4b5f4a0760d4a16080cfc
jayden-lee <jaydenlsf47@gmail.com>
2021-05-05 09:13:03 +0100

    Cleaned up fibo function

--- daily-challenge-5/fibo.py
+++ daily-challenge-5/fibo.py
@@ -3,9 +3,7 @@ def fibo(n):
     if n < 0:
         return ((-1) ** (n + 1)) * fibo(-n)
 
-    if n == 1:
-        return n
-    elif n == 0:
+    if n == 0 or n == 1:
         return n
     else:
         return fibo(n - 1) + fibo(n - 2)
